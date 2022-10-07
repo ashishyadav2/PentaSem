866fc3185d08f2c7ee45c740591168caa4f84297
Ashish Yadav <unixm98@gmail.com>
2022-10-07 19:49:09 +0530

    new update

--- Project/app.py
+++ Project/app.py
@@ -1,7 +1,6 @@
-from crypt import methods
+from pydoc import classname
 from flask import Flask, escape, request, render_template
 import pickle
-from sklearn.feature_extraction.text import TfidfVectorizer
 
 vector = pickle.load(open("vectorizer.pkl",'rb'))
 
@@ -16,12 +15,15 @@ def home():
 @app.route('/prediction', methods=['GET','POST'])
 def prediction():
     if request.method == "POST":
-        #input type text ka name news hai
         news = str(request.form['news'])
         predict = model.predict(vector.transform([news]))[0]
-        print(predict)
+ 
+        if predict == 'REAL':
+            class_name = "real"        
+        else:
+            class_name = "fake"
 
-        return render_template("prediction.html",prediction_text = "News Headline is: {}".format(predict))
+        return render_template("prediction.html",prediction_text = "The News headline is predicted as {}".format(predict),class_name = class_name)
 
     else:
         return render_template("prediction.html")
---
+++ Project/readme.txt
@@ -0,0 +1,22 @@
+#prerequisite
+python 3
+vs code
+pip install jupyter notebook
+
+#for demo of flask project
+newsapp/scripts/activate
+python app.py
+
+#news headlines
+real
+Kerry to go to Paris in gesture of sympathy
+The Battle of New York: Why This Primary Matters
+Fact check: Trump and Clinton at the 'commander-in-chief' forum
+Iran reportedly makes new push for uranium concessions in nuclear talks
+With all three Clintons in Iowa, a glimpse at the fire that has eluded Hillary Clintonâ€™s campaign
+Donald Trumpâ€™s Shockingly Weak Delegate Game Somehow Got Even Worse
+
+fake
+You Can Smell Hillaryâ€™s Fear
+Watch The Exact Moment Paul Ryan Committed Political Suicide At A Trump Rally (VIDEO)
+Bernie supporters on Twitter erupt in anger against the DNC: 'We tried to warn you!'
