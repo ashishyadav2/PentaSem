2df2682f9130811b502172a0151942e8ca2d3c43
Ashish Yadav <unixm98@gmail.com>
2022-11-06 20:46:10 +0530

    api update

--- Project/app.py
+++ Project/app.py
@@ -1,6 +1,5 @@
-from pydoc import classname
 from flask import Flask, escape, request, render_template
-import pickle
+import pickle, requests, json
 
 vector = pickle.load(open("vectorizer.pkl",'rb'))
 
@@ -17,13 +16,22 @@ def prediction():
     if request.method == "POST":
         news = str(request.form['news'])
         predict = model.predict(vector.transform([news]))[0]
- 
+
+        # api request call
+        url = 'https://newsapi.org/v2/everything?q={}&apiKey=8535a18a149149e59842155bfa13ec8d'.format(news)
+        r = requests.get(url)
+        data = json.loads(r.content)
+        urlLink = []
+        for i in range(len(data['articles'])):
+            urlLink.append(data['articles'][i]['url'])
+        # api call end
+
         if predict == 'REAL':
             class_name = "real"        
         else:
             class_name = "fake"
 
-        return render_template("prediction.html",prediction_text = "The News headline is predicted as {}".format(predict),class_name = class_name)
+        return render_template("prediction.html",prediction_text = "The News headline is predicted as {}".format(predict),class_name = class_name,url_list = urlLink)
 
     else:
         return render_template("prediction.html")
